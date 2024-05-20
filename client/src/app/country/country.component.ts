750837bac35be2dd35e7e49577adbfb9739eee8d
efrat <efrat.shefer@agamimp.net>
2024-05-20 12:48:51 +0300

    add double click on row

--- client/src/app/country/country.component.ts
+++ client/src/app/country/country.component.ts
@@ -89,4 +89,9 @@ export class CountryComponent {
 	compare(a: number | string, b: number | string, isAsc: boolean) {
 		return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 	}
+
+	clickedRow(row: ICountry) {
+		console.log(`row: ${row}`);
+		
+	}
 }
